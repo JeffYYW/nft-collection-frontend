ee76d42c8ea57ba6768445fb7158953ecd6b6c66
Jeffrey Yeung <jeffyyw@gmail.com>
2022-08-25 14:22:07 +0800

    minting functions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b83a32775f9c23c"; }
+/******/ 		__webpack_require__.h = function() { return "5b78ddbfa4a92fdb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
